aeef2c822ab72e71de460543a05235c01884b049
Atanas Bahchevanov <a.bahchevanov@yahoo.com>
2020-03-04 21:06:06 +0200

    “init”

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,15 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://github.com/abahchevanov/daplast-bg.git
+ * Copyright 2020. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
